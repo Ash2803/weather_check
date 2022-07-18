10311de9945bc2c728ccfb7db1f44839c07c0814
Ashot Abazyan <ashot.93@mail.ru>
2022-07-18 21:25:56 +0300

    18/07

--- weather_request.py
+++ weather_request.py
@@ -2,10 +2,13 @@ import requests
 
 
 def get_url():
-    url_template = f'https://wttr.in/{"Череповец"}'
-    response = requests.get(url_template)
-    response.raise_for_status()
-    return response.text
+    cities = ('Череповец', 'Лондон', 'Шереметьево')
+    lang = "lang=ru"
+    for city in cities:
+        url_template = f'https://wttr.in/{city}'
+        response = requests.get(url_template, params=lang)
+        response.raise_for_status()
+        print(response.text)
 
 
 print(get_url())
