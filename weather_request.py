3cece02ef2f54b945927ff2a6c272ba020bf4b18
Ashot Abazyan <ashot.93@mail.ru>
2022-07-20 22:19:21 +0300

    renamed variables and made them easy to understand

--- weather_request.py
+++ weather_request.py
@@ -1,14 +1,19 @@
 import requests
 
 
-def get_url():
+def get_current_weather():
     cities = ('Череповец', 'Лондон', 'Шереметьево')
-    lang = {'lang': 'ru'}
+    request_parameters = {'lang': 'ru'}
     for city in cities:
         url_template = f'https://wttr.in/{city}'
-        response = requests.get(url_template, params=lang)
+        response = requests.get(url_template, params=request_parameters)
         response.raise_for_status()
         print(response.text)
 
 
-print(get_url())
+def main():
+    print(get_current_weather())
+
+
+if __name__ == '__main__':
+    main()
